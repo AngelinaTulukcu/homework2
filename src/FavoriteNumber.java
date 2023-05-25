14eca595817bbc975845bd52de0ba9607e306b8a
Angelina <a.tulukcu@teamprocure.com>
2023-05-25 18:08:21 +0300

    Add blue, green, and red colors

---
+++ src/FavoriteNumber.java
@@ -0,0 +1,6 @@
+public class FavoriteNumber  {
+    public static void main(String[] args) {
+        System.out.println(42);
+    }
+}
+
---
+++ src/NewClass.java
@@ -0,0 +1,7 @@
+public class NewClass {
+    public static class StarPrinting {
+        public static void main(String[] args) {
+            System.out.println("*");
+        }
+    }
+}
